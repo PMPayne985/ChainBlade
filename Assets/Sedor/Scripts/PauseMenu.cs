d9ca9fc78f034340757d29be2ae3b4fc9c91aa07
Eric-Kyle-Sedor <69442425+Eric-Kyle-Sedor@users.noreply.github.com>
2022-07-23 11:42:49 -0400

    Modified Pause Menu
    Edited pause menu script to not be blank anymore, did some basic setup of the prefab.

--- Assets/Sedor/Scripts/PauseMenu.cs
+++ Assets/Sedor/Scripts/PauseMenu.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public bool optionsBool;
+    public GameObject mainPause;
+    public GameObject optionsPause;
+    public Slider rotationSpeed;
+    public Toggle rotateDirect;
+    public Slider music;
+    public Slider sfx;
+    public Slider master;
+    public int RotationSpeedFinal;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +26,36 @@ public class PauseMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MainMenuLoad()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Options()
+    {
+        optionsBool = !optionsBool;
+
+        if (!optionsBool)
+        {
+            mainPause.SetActive(true);
+            optionsPause.SetActive(false);
+        }
+        else
+        {
+            mainPause.SetActive(false);
+            optionsPause.SetActive(true);
+
+            Debug.Log($"Rotation Speed: {rotationSpeed.value}");
+            Debug.Log($"Music Sound: {music.value}");
+            Debug.Log($"SFX Sound: {sfx.value}");
+            Debug.Log($"Master Sound: {master.value}");
+        }
+
+    }
+    public void InverseRotate()
+    {
+        RotationSpeedFinal = RotationSpeedFinal * -1;
     }
 }
