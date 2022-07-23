a3dc86a209a221eea556b8f522a7544dff153784
PMPayne985 <pmpayne985@outlook.com>
2022-07-23 13:09:27 -0400

    Update TestScript.cs

--- Assets/Zer0/Scripts/TestScript.cs
+++ Assets/Zer0/Scripts/TestScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    public float theAnswer = 42;
+    public float theAnswer = 51;
     // Start is called before the first frame update
     void Start()
     {
