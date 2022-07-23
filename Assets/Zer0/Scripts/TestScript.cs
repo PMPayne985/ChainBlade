3cef0612223316ccfb29a37412c687a9c4e28e85
PMPayne985 <pmpayne985@outlook.com>
2022-07-23 13:08:31 -0400

    Update TestScript.cs

--- Assets/Zer0/Scripts/TestScript.cs
+++ Assets/Zer0/Scripts/TestScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    public float theAnswer = 37;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"The answer to the universe, life, and everything else is {theAnswer}");
     }
 
     // Update is called once per frame
